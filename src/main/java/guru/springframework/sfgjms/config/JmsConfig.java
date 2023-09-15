87ec437d11e1bc1a87672e9c1c897199a575a915
Froylan Villaverde Gonzalez <froy.villaverde@gmail.com>
2023-09-15 19:33:10 -0400

    # Updated Jms Config, sneder.

--- src/main/java/guru/springframework/sfgjms/config/JmsConfig.java
+++ src/main/java/guru/springframework/sfgjms/config/JmsConfig.java
@@ -8,8 +8,11 @@ import org.springframework.jms.support.converter.MessageType;
 @Configuration
 public class JmsConfig {
 
+    public static final String MY_QUEUE = "my-hello-world";
+
     public MessageConverter messageConverter(){
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
         return converter;
     }
